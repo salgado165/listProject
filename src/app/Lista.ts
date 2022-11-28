418f9d7cd20b6e44b25af7a5f5cc99b00b046981
Carlos Daniel Salgado <salgado165@hotmail.com>
2022-11-28 10:21:06 -0600

    project list

---
+++ src/app/Lista.ts
@@ -0,0 +1,5 @@
+export default interface List {
+    id: number,
+    description : string,
+    date?: Date
+}
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import List from '../../Lista';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  allItems: List[] = [
+    {id:1, description: "Create Angular Project" },
+    {id:2, description: "Learn Bootstrap" },
+    {id:3, description: "Get Angular Router Information" },
+    {id:4, description: "Implement Router" },
+    {id:5, description: "Improve Bootstrap Implementation" },
+  ];
 
+  get items(){
+    return this.allItems;
+  }
 }
