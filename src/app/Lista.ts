3f35874d9c3dc68bde219bc7d1ffd678e1296643
Carlos Daniel Salgado <salgado165@hotmail.com>
2022-11-29 18:06:37 -0600

    data list

--- src/app/Lista.ts
+++ src/app/Lista.ts
@@ -1,5 +1,6 @@
 export default interface List {
     id: number,
+    title: string,
     description : string,
     date?: Date
 }
--- src/app/data.ts
+++ src/app/data.ts
@@ -1,10 +1,10 @@
 const dataproject = [
-    {id:1, description: "Create Angular Project" },
-    {id:2, description: "Learn Bootstrap" },
-    {id:3, description: "Get Angular Router Information" },
-    {id:4, description: "Implement Router" },
-    {id:5, description: "Improve Bootstrap Implementation" },
-    {id:6, description: "Import data from file" },
+    {id:1, title: 'Angular', description: "Create Angular Project" },
+    {id:2, title: 'CSS', description: "Learn Bootstrap" },
+    {id:3, title: 'Router Angular', description: "Get Angular Router Information" },
+    {id:4, title: 'Router Implementation', description: "Implement Router" },
+    {id:5, title: 'Bootstrap', description: "Improve Bootstrap Implementation" },
+    {id:6, title: 'Import Data', description: "Import data from file" },
 ]
 
 export default dataproject;
