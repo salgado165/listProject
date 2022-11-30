3a3e9770d5ee2dc5832e70781ce1b73e96ce0548
Carlos Daniel Salgado <salgado165@hotmail.com>
2022-11-30 13:06:12 -0600

    update data

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -1,7 +1,7 @@
 import {ListProj} from './ListProj';
 export let dataProject: ListProj[]= [
-    {id:1, title: 'Lavado de Activos', description: "Implementación de sistema de lavado de activos,  el cual incluye el procesamiento de archivos para el nivel de riesgo de clientes" },
-    {id:2, title: 'Recursos Humanos', description: "Sistema de gestión de horas extras" },
+    {id:1, title: 'FATCA', description: "Implementación de sistema de prevención de lavado de activos,  el cual incluye el procesamiento de archivos para calcular el nivel de riesgo de clientes" },
+    {id:2, title: 'Recursos Humanos', description: "Sistema de gestión y monitoreo de horas extras" },
     {id:3, title: 'Control De Producción', description: "Sistema de procesamiento de archivos, con el proposito de medir el nivel de producción y agilizar el proceso de proyección de horas hombres necesaria para los nuevos requisitos de trabajo" },
     
 ]
