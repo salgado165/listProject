39c00cf05d48f3cfd996bae4d3e11d4ab70cd578
Carlos Daniel Salgado <salgado165@hotmail.com>
2022-11-30 10:14:51 -0600

    idioma, topbar y listas

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -1,9 +1,7 @@
 import {ListProj} from './ListProj';
 export let dataProject: ListProj[]= [
-    {id:1, title: 'Angular', description: "Create Angular Project" },
-    {id:2, title: 'CSS', description: "Learn Bootstrap" },
-    {id:3, title: 'Router Angular', description: "Get Angular Router Information" },
-    {id:4, title: 'Router Implementation', description: "Implement Router" },
-    {id:5, title: 'Bootstrap', description: "Improve Bootstrap Implementation" },
-    {id:6, title: 'Import Data', description: "Import data from file" },
+    {id:1, title: 'Lavado de Activos', description: "Implementación de sistema de lavado de activos,  el cual incluye el procesamiento de archivos para el nivel de riesgo de clientes" },
+    {id:2, title: 'Recursos Humanos', description: "Sistema de gestión de horas extras" },
+    {id:3, title: 'Control De Producción', description: "Sistema de procesamiento de archivos, con el proposito de medir el nivel de producción y agilizar el proceso de proyección de horas hombres necesaria para los nuevos requisitos de trabajo" },
+    
 ]
